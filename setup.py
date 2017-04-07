ff144a750f32bf012b6dd6e97e087efbfea35d3a
alexandermlabs <alexandermlabs@yandex.ru>
2017-04-07 20:33:16 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='alexandermlabs',
+      version='0.1',
+      description='Test',
+      url='https://github.com/alexandermlabs',
+      author='me',
+      author_email='alexandermlabs@yandex.ru',
+      license='GPL',
+      packages=['alexandermlabs'],
+      zip_safe=False)
